1ee8d166173936c172c9d0c48865da2e786ade0f
amit-82 <leshem.amit@gmail.com>
2020-04-16 17:10:43 +0300

    rendering polyline and polygon

--- example/src/index.js
+++ example/src/index.js
@@ -2,6 +2,8 @@ const {
 	PathController,
 	createSVGElement,
 	LineController,
+	PolylineController,
+	PolygonController,
 } = require("../../lib/index");
 
 const svgElem = document.getElementById("svg");
@@ -19,3 +21,20 @@ new LineController(lineElem).moveTo(10, 10).lineTo(20, 30).updateElement();
 
 const lineElem2 = createSVGElement("line", svgElem);
 new LineController(lineElem2).lineTo(10, 60).updateElement();
+
+const polyline = createSVGElement("polyline", svgElem);
+new PolylineController(polyline)
+	.moveTo(30, 5)
+	.lineTo(40, 35)
+	.lineTo(35, 35)
+	.lineTo(80, 80)
+	.lineTo(35, 80)
+	.updateElement();
+
+const polygon = createSVGElement("polygon", svgElem);
+new PolygonController(polygon)
+	.moveTo(250, 120)
+	.lineTo(300, 150)
+	.lineTo(350, 120)
+	.lineTo(300, 200)
+	.updateElement();
